fb30346ee23cb92c8e35c155b7d213e27b7cb43c
azcoigreach <azcoigreach@gmail.com>
2017-12-21 05:21:45 -0700

    debug

--- app/led_master.py
+++ app/led_master.py
@@ -324,7 +324,7 @@ if __name__ == "__main__":
         for app in apps:
             p = Process(target=app)
             p.start()
-            processes[n] = (p, app) # Keep the process and the app to monitor or restart
+            # processes[n] = (p, app) # Keep the process and the app to monitor or restart
             n += 1
 
         # while len(processes) > 0:
