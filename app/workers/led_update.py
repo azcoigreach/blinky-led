923b50a8cc29ce9fcc6b69ccddcaf848dabbf8ee
azcoigreach <azcoigreach@gmail.com>
2017-12-21 10:48:34 -0700

    mp manager debug

--- app/workers/led_update.py
+++ app/workers/led_update.py
@@ -78,7 +78,7 @@ class RunText(BlinkyBase):
 
 def led_update(d):
     logger.debug(d)
-    # global d_time_now, d_count_down, d_curr_tweet, d_curr_temp
+    global d_time_now, d_count_down, d_curr_tweet, d_curr_temp
     
     d_time_now = d['time_now']
     d_count_down = d['count_down']
