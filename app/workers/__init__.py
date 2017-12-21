fd203aa60599103f4a68d78067f39ab55ca1b537
azcoigreach <azcoigreach@gmail.com>
2017-12-21 06:50:40 -0700

    refactoring

--- app/workers/__init__.py
+++ app/workers/__init__.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
 # importing all files from folder
 import os
 import glob
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('led_master')
+
 __all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
