266976ae01f4faa304fcbf13147141a163c4e026
azcoigreach <azcoigreach@gmail.com>
2017-12-21 09:03:40 -0700

    debug d

--- app/workers/led_update.py
+++ app/workers/led_update.py
@@ -77,7 +77,7 @@ class RunText(BlinkyBase):
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
 
 def led_update(d):
-    pprint.pprint(d[time_now])
+    pprint.pprint(d['time_now'])
     
     
     parser = RunText(d)
