b61a7e0190641d95b0dc4e31ec4bf2c967f1a24d
azcoigreach <azcoigreach@gmail.com>
2017-12-23 19:57:35 -0700

    led redux with pickles

--- app/led_master.py
+++ app/led_master.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python 2.7
-import time
 from time import strftime
-import os
-from multiprocessing import Process, Manager, Lock, Array, Value
-import pprint
 from blinkybase import BlinkyBase
 from rgbmatrix import graphics
+import time
+from multiprocessing import Process, Lock
+from multiprocessing.sharedctypes import Value, Array
 import datetime
-
+import feedparser, bitly_api
+import urllib2
+import json
+import os
+import threading
+from PIL import Image, ImageFont, ImageDraw
+import random
+import pickle
 from workers import *
 import logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
+logger = logging.getLogger('MASTER')
+logger.setLevel(logging.INFO)
 
 class RunText(BlinkyBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-        logger.warning('Init: BlinkyBase')
+        logger.warning('Init: LED Loop')
 
     def Run(self):
+        
         
         offscreenCanvas = self.matrix.CreateFrameCanvas()
         
@@ -50,55 +56,70 @@ class RunText(BlinkyBase):
         
         led_width = offscreenCanvas.width
         
+        self.curr_temp = ''
+        self.curr_tweet = ''
+
         while True:
+
+            try:
+                self.weather = pickle.load(open('weather.pickle', 'rb'))
+                logger.debug('Weather Pickle: %s', self.weather)
+                self.curr_temp = self.weather['curr_temp']
+
+                self.twitter = pickle.load(open('twitter.pickle', 'rb'))
+                logger.debug('Twitter Pickle: %s', self.twitter)
+                self.curr_tweet = self.twitter["curr_tweet"]
+
+            except Exception as err:
+                logger.error('Pickle Error: %s', err)
+                
+
             offscreenCanvas.Clear()
-            self.clock = d_time_now
-            self.count_down = d_count_down
-            
-            #Lines
+            self.clock = time_now.value
+            self.count_down = count_down.value
+            graphics.DrawText(offscreenCanvas, count_font, 1, 31, count_color, self.count_down)
             graphics.DrawLine(offscreenCanvas, 0, 18, 128, 18, line_a_color)
             graphics.DrawLine(offscreenCanvas, 68, 19, 68, 32, line_a_color)
+            graphics.DrawText(offscreenCanvas, time_font, 71, 31, time_color, self.clock)
+            graphics.DrawText(offscreenCanvas, count_label_font, 1, 25, count_label_color, 'NOT MY PRESIDENT!')
+            graphics.DrawText(offscreenCanvas, count_wx_font, 104, 25, count_wx_color, self.curr_temp)
             
             # Top Twitter Ticker
-            if d_curr_tweet != '': 
-                logger.debug(d_curr_tweet)
-                ticker = d_curr_tweet
-                len = graphics.DrawText(offscreenCanvas, ticker_font, self.pos, 14, ticker_color, ticker)
-                self.pos -= 1
-                if (self.pos + len < 0):
-                    self.pos = offscreenCanvas.width
+        
+            len = graphics.DrawText(offscreenCanvas, ticker_font, self.pos, 14, ticker_color, self.curr_tweet)
+            self.pos -= 1
+            if (self.pos + len < 0):
+                self.pos = offscreenCanvas.width
             
-            #Bottom Right Clock
-            graphics.DrawText(offscreenCanvas, time_font, 71, 31, time_color, self.clock)
-            
-            #Bottom Left Countdown Clock
-            graphics.DrawText(offscreenCanvas, count_font, 1, 31, count_color, self.count_down)
-            graphics.DrawText(offscreenCanvas, count_label_font, 1, 25, count_label_color, 'NOT MY PRESIDENT!')
-            
-            # Bottom Right Weather Bug
-            self.temp = str('%sF') % d_curr_temp
-            graphics.DrawText(offscreenCanvas, count_wx_font, 104, 25, count_wx_color, self.temp)
-            
-            # Refresh Rate    
             time.sleep(0.025)
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
 
-def led_clock():
-    logger.warning('Init: Clock function')
-    while True:
-        dt = datetime.datetime
-        dt_now = dt.now().strftime('%m/%d/%y %H:%M')
-        d['time_now'] = str(dt_now)
-        logger.debug('Current Time: %s',d['time_now'])
-        
-        time.sleep(10)
-
 def led_update():
-    logger.debug('Init: led_update function')
-    led_parser = RunText()
+    parser = RunText()
+    if (not parser.process()):
+        parser.print_help()
+        
+
+class led_clock():
+    def __init__(self, *args, **kwargs):
+        while True:
+            dt = datetime.datetime
+            t = dt.now()
+            time_now.value = t.strftime('%m/%d/%y %H:%M')
+            #print(time_now.value)
+            time.sleep(5)
+
+
+class countdown_clock():
+    def __init__(self, *args, **kwargs):
+        while True:
+            dt = datetime.datetime
+            count = dt(2021,1,21,9) - dt.now()
+            count_down.value = '%dDays %dH %dM' % (count.days, count.seconds/3600, count.seconds%3600/60)
+            #print(count_down.value)
+            time.sleep(5)
     
-
-
+            
 def main():
     pass
 
@@ -112,12 +133,17 @@ if __name__ == "__main__":
         #Process Variables
         time_now = Array('c', b'88/88/88 88:88' ,lock=lock) 
         count_down = Array('c', b'8888Days 88H 88M' ,lock=lock)
-        curr_temp = Value('d', 888.8)
-        news_ticker = Array('c', b'9999.ppm' ,lock=lock)
-        news_ticker.value = str('0.ppm')
-        ticker_ready = Value('i', 0)
-        
-        
+
+        #Start TWITTER WORKER
+        rt = Process(target=twitter.tweet_query)
+        jobs.append(rt)
+        rt.start()
+
+        #Start WEATHER WORKER
+        rt = Process(target=weather.get_temp)
+        jobs.append(rt)
+        rt.start()
+
         #Start LED_CLOCK LOOP
         rt = Process(target=led_clock)
         jobs.append(rt)
@@ -127,17 +153,7 @@ if __name__ == "__main__":
         rt = Process(target=countdown_clock)
         jobs.append(rt)
         rt.start()
-        
-        #Start Weather Updater
-        rt = Process(target=weather)
-        jobs.append(rt)
-        rt.start()
-        
-        #Start RSS Feed Updater
-        # rt = Process(target=rss_feed)
-        # jobs.append(rt)
-        # rt.start()
-        
+                
         #Start LED UPDATE LOOP
         rt = Process(target=led_update)
         jobs.append(rt)
