ad10c12aef75ce7dcde4c5f814674ca46d0f49cf
azcoigreach <azcoigreach@gmail.com>
2017-12-23 20:10:05 -0700

    led_redux commit to master

--- app/led_master.py
+++ app/led_master.py
@@ -125,38 +125,29 @@ def main():
 
 if __name__ == "__main__":
     try:
-        logger.warning('Work Started: PID %d', os.getpid())
-        with Manager() as manager:
-            d = manager.dict()
-            d = {'time_now': b'88/88/88 88:88', 'count_down': b'8888Days 88H 88M', 'curr_temp':888.8, 'news_ticker':b'0.ppm', 'curr_tweet':b'screen_name: xxxx : ddd mmm DD HH:MM:SS +0000 YYYY'}
-            logger.debug(d)
-            apps = [led_update.RunText] #, tweet_query.tweet_query, led_clock.led_clock, countdown_clock.countdown_clock, weather.weather] 
-            processes = {}
-            n=0
-            for app in apps:
-                instance = app(d)
-                p = Process(target=instance, args=(d,))
-                p.start()
-                processes[n] = (p, app)
-                n += 1
-                logger.warning('Procs Started: %s ', p, p.is_alive())
-                time.sleep(0.25)
+        print 'Work Started: PID %d' % os.getpid()
+        
+        jobs = []
+        lock = Lock()
+        
+        #Process Variables
+        time_now = Array('c', b'88/88/88 88:88' ,lock=lock) 
+        count_down = Array('c', b'8888Days 88H 88M' ,lock=lock)
 
-            while len(processes) > 0:
-                for n in processes.keys():
-                    (p, a) = processes[n]
-                    time.sleep(0.5)
-                    if p.exitcode is None and not p.is_alive(): # Not finished and not running
-                        # Do your error handling and restarting here assigning the new process to processes[n]
-                        logger.warning('is gone as if never born!',a)
-                    elif p.exitcode < 0:
-                        logger.warning('Process Ended with an error or a terminate', a)
-                        # Handle this either by restarting or delete the entry so it is removed from list as for else
-                    else:
-                        logger.warning('finished', a)
-                        p.join() # Allow tidyup
-                        del processes[n] # Removed finished items from the dictionary 
-                        # When none are left then loop will end
+        #Start TWITTER WORKER
+        rt = Process(target=twitter.tweet_query)
+        jobs.append(rt)
+        rt.start()
+
+        #Start WEATHER WORKER
+        rt = Process(target=weather.get_temp)
+        jobs.append(rt)
+        rt.start()
+
+        #Start LED_CLOCK LOOP
+        rt = Process(target=led_clock)
+        jobs.append(rt)
+        rt.start()
         
         #Start LED_CLOCK LOOP
         rt = Process(target=countdown_clock)
