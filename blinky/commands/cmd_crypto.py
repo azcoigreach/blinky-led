8c63ada93157bc93d0ccf31a4fb937959d04c11e
azcoigreach <azcoigreach@gmail.com>
2021-11-23 16:03:48 -0700

    Price range bar

--- blinky/commands/cmd_crypto.py
+++ blinky/commands/cmd_crypto.py
@@ -7,10 +7,23 @@ import requests
 
 @pass_environment
 def binance(ctx, symbol):
-    response = requests.get(f'https://api.binance.us/api/v3/ticker/24hr?symbol={symbol}')
-    ctx.json_data = response.json()
-    ctx.vlog(click.style(ctx.json_data, fg="red"))
-    return 
+    try:
+        response = requests.get(f'https://api.binance.us/api/v3/ticker/24hr?symbol={symbol}')
+        ctx.json_data = response.json()
+        ctx.vlog(click.style(ctx.json_data, fg="red"))
+    except Exception as e :
+        err_refesh = 15
+        ctx.log(click.style(f'Error: {e}', fg='red'))
+        ctx.log(click.style(f'Trying again in {err_refesh}s', fg='yellow'))
+        time.sleep(err_refesh)
+
+@pass_environment
+def markPriceRange(ctx):
+    matrix_width = 126
+    pixel_price = ((float(ctx.json_data['highPrice'])-float(ctx.json_data['lowPrice']))/matrix_width)
+    # ctx.vlog(click.style(f'pixel_price = {pixel_price}', fg='yellow'))
+    ctx.curr_pixel = int(round(((float(ctx.json_data['lastPrice'])-float(ctx.json_data['lowPrice'])))/pixel_price))
+    ctx.vlog(click.style(f'curr_pixel = {ctx.curr_pixel}', fg='yellow'))
 
 @click.option(
     "-s",
@@ -45,8 +58,8 @@ def cli(ctx, symbol, refresh):
         fnt_big = ImageFont.load(f"/home/pi/BlinkyLED/blinky/fonts/10x20.pil")
         color_a = (194, 112, 29) # orange
         color_b = (29, 167, 194) # cyan
-        color_c = (194, 29, 29) # red
-        color_d = (29, 194, 45) # green
+        color_c = (145, 29, 23) # red
+        color_d = (25, 145, 23) # green
         color_e = (142, 35, 161) # purple
         binance(symbol)     
         draw = ImageDraw.Draw(image)
@@ -55,6 +68,10 @@ def cli(ctx, symbol, refresh):
         draw.text((1, 15), str(f"${float(ctx.json_data['lastPrice'])}"), font=fnt_big, fill=color_b)
         draw.text((65, 1), str(f"${float(ctx.json_data['priceChange'])}"), font=fnt_small, fill=color_e)
         draw.text((65, 7), str(f"{float(ctx.json_data['priceChangePercent'])}%"), font=fnt_small, fill=color_e)
+        markPriceRange()
+        draw.rectangle((1,14,(ctx.curr_pixel-1),15), fill=color_d) # Low Range
+        draw.rectangle(((ctx.curr_pixel+1),14,126,15), fill=color_c) # High range
+        draw.rectangle((ctx.curr_pixel,13,ctx.curr_pixel,16), fill=color_b) # Current value
         # draw.text((1,22), ctx.json_data['highPrice'], font=fnt_small, fill=color_a)
         # draw.text((20,22), ctx.json_data['lowPrice'], font=fnt_small, fill=color_a)
         ctx.vlog(click.style("Matrix Set", fg="red"))  
